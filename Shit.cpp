f019b6f0e7548e1f8e0c969f37da4f3254997374
WeLikeFn <93561180+WeLikeFn@users.noreply.github.com>
2021-11-01 20:32:28 -0500

    Update Shit.cpp

--- Shit.cpp
+++ Shit.cpp
@@ -1,24 +1,27 @@
-			for (int li = 0; li < Readtard<DWORD>(Uworld + 0x140 + sizeof(PVOID)); ++li) {
-				DWORD_PTR levels = Readtard<uintptr_t>(Uworld + 0x140);
-				if (!levels) break;
+void TeleportToMarker()
+{
+	for (int li = 0; li < Readtard<DWORD>(Uworld + 0x140 + sizeof(PVOID)); ++li) {
+		DWORD_PTR levels = Readtard<uintptr_t>(Uworld + 0x140);
+		if (!levels) break;
 
-				DWORD_PTR level = Readtard<uintptr_t>(levels + li * sizeof(PVOID));
+		DWORD_PTR level = Readtard<uintptr_t>(levels + li * sizeof(PVOID));
 
-				if (!level) std::cout << "Failed LEVEL\n";
+		if (!level) std::cout << "Failed LEVEL\n";
 
-				auto lc = Readtard<DWORD>(level + offset_aactors + sizeof(PVOID));
+		auto lc = Readtard<DWORD>(level + offset_aactors + sizeof(PVOID));
 
-				DWORD_PTR AActorsLEVE = Readtard<uintptr_t>(level + offset_aactors);
+		DWORD_PTR AActorsLEVE = Readtard<uintptr_t>(level + offset_aactors);
 
-				for (int ai = 0; ai < lc; ++ai)
-				{
-					uint64_t Acotr = Readtard<uint64_t>(AActorsLEVE + ai * sizeof(PVOID));
-					//Get Name
-					if (Name.find("AthenaPlayerMarker") != std::string::npos)
-					{
-						auto Rootcomp = Readtard<uint64_t>(Acotr + 0x130);
-						auto Pos = Readtard<Vector3>(Rootcomp + 0x11C);
-						Teleport(Pos);
-					}
-				}
+		for (int ai = 0; ai < lc; ++ai)
+		{
+			uint64_t Acotr = Readtard<uint64_t>(AActorsLEVE + ai * sizeof(PVOID));
+			//Get Name
+			if (Name.find("AthenaPlayerMarker") != std::string::npos)
+			{
+				auto Rootcomp = Readtard<uint64_t>(Acotr + 0x130);
+				auto Pos = Readtard<Vector3>(Rootcomp + 0x11C);
+				Teleport(Pos);
 			}
+		}
+	}
+}
